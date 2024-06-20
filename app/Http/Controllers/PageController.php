cdc27d492aa850a3e178b66cd651e0a6324906e4
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-06-20 16:43:26 +0300

    created the student and grade route

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class PageController extends Controller
 {
     public function home()
@@ -40,5 +41,50 @@ class PageController extends Controller
     {
         return "This is the galary page";
     }
-    
+
+    public function student($adm){
+        $students = [
+            '123' => 'Dennis',
+            '124' => 'Stacy',
+            '125' => 'Lawrence', 
+            '126' => 'Jackline',
+            '127' => 'Juliana' ,  
+        ];
+        if(array_key_exists($adm, $students)){
+         return $students[$adm];
+        }else{
+            return 'student: '.$adm.' not found';
+        
+        }
+    }
+    //route that takes a parameter marks. when key in marks it returns the grade(grade/80)
+    public function grade($marks){
+        if($marks >= 46 && $marks <= 50){
+            return 'A+';
+        }elseif($marks >= 41 && $marks <= 45){
+            return 'A';
+        }elseif($marks >= 36 && $marks <= 40){
+            return 'B+';
+        }elseif($marks >= 31 && $marks <= 35){
+            return 'B';
+        }elseif($marks >= 26 && $marks <= 30){
+            return 'C+';
+        }elseif($marks >= 21 && $marks <= 25){
+            return 'C';
+        }elseif($marks >= 16 && $marks <= 20){
+            return 'D+';
+        }elseif($marks >= 11 && $marks <= 15){
+            return 'D';
+        }elseif($marks >= 6 && $marks <= 10){
+            return 'E';
+        }else{
+            return "Invalid marks";
+        }
+    }
 }
+/* 
+Controllers are meant to handle the logic of your application. They are used to handle user requests and return appropriate responses.
+The PageController class is a controller class that extends the base controller class.
+Each method in the PageController class corresponds to a route defined in the routes/web.php file.
+when a request is made to a route, the corresponding method in the controller class is called to handle the request.(the method returns a response that is sent back to the user)
+*/
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;//? importing the Route class from the Illuminate\Support\Facades namespace
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -15,5 +15,17 @@ Route::namespace('App\Http\Controllers')->group(function () {
     Route::get('/blog', 'PageController@blog')->name('blog');
     Route::get('/products', 'PageController@products')->name('products');
     Route::get('/galary', 'PageController@galary')->name('galary');
+    Route::get('/student/{adm}', 'PageController@student')->name('student');
+    Route::get('/grade/{marks}', 'PageController@grade')->name('grade');
     
 });
+
+/* 
+The Route::namespace method is used to specify the namespace of the controller class.(prefixing the controller class with the App\Http\Controllers namespace)
+The Route::group method is used to group the routes that share the same namespace/characteristic ex. Route::get
+Each Route::get method defines a route that responds to HTTP GET requests
+The first argument of the Route::get method is the URI of the route
+The second argument of the Route::get method is the controller method that should be called when the route is matched/The action that should be taken when the route is matched
+-format 'ControllerName@methodName'
+The name method is used to give the route a name that can be used to generate URLs to the route
+ */
